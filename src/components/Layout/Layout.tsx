98cf320aa32fba88c60536813c6bb69c0ea45bc8
Ruben Mesta <rubenmesta29@gmail.com>
2023-01-18 17:08:33 -0500

    use PropsWithChildren instead

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,13 @@
 import Box from "@mui/material/Box/Box";
 import { ReactNode } from "react";
 import Navbar from "../Navbar/NavBar";
+import { PropsWithChildren } from "react";
 
-type Props = {
+interface Props {
   children?: ReactNode;
-};
+}
 
-export const Layout = ({ children }: Props) => {
+export const Layout = ({ children }: PropsWithChildren<Props>): JSX.Element => {
   return (
     <Box sx={{ maxWidth: "420px", margin: "0 auto" }}>
       <Navbar />
